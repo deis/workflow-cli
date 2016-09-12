8456ad4e91f549bd3c7b4d4ea66bb30f9090c84d
Matt Tucker <mtucker@deis.com>
2016-09-12 09:13:13 -0600

    fix(tests): no longer reset home env vars.

--- settings/home_unix_test.go
+++ settings/home_unix_test.go
@@ -11,26 +11,16 @@ import (
 
 // TestFindHome ensures the correct home directory is returned by FindHome().
 func TestFindHome(t *testing.T) {
-	previousHome := os.Getenv("HOME")
-
 	expectedHomeDir := "/d/e/f"
 	os.Setenv("HOME", expectedHomeDir)
 
 	assert.Equal(t, FindHome(), expectedHomeDir, "output")
-
-	// Reset HOME.
-	os.Setenv("HOME", previousHome)
 }
 
 // TestSetHome ensures the correct env vars are set when SetHome() is called.
 func TestSetHome(t *testing.T) {
-	previousHome := os.Getenv("HOME")
-
 	homeDir := "/a/b/c"
 	SetHome(homeDir)
 
 	assert.Equal(t, os.Getenv("HOME"), homeDir, "output")
-
-	// Reset HOME.
-	os.Setenv("HOME", previousHome)
 }
