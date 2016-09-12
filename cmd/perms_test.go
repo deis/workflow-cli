19cd4bb21e7233bfb4dc545492f76372898bd9cd
Matt Tucker <mtucker@deis.com>
2016-09-12 15:45:51 -0600

    tests(perms): add tests for 'perms:create' and 'perms:delete'.

--- cmd/perms_test.go
+++ cmd/perms_test.go
@@ -144,3 +144,111 @@ func TestPermsListAdminsLimit(t *testing.T) {
 fred
 `, "output")
 }
+
+func TestPermsCreateUser(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+  "uuid": "c4aed81c-d1ca-4ff1-ab89-d2151264e1a3",
+  "id": "lorem-ipsum",
+  "owner": "dolar-sit-amet",
+  "structure": {
+    "cmd": 1
+  },
+  "created": "2016-08-22T17:40:16Z",
+  "updated": "2016-08-22T17:40:16Z"
+}`)
+	})
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/perms", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+	})
+
+	err = cmdr.PermCreate("lorem-ipsum", "test-user", false)
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Adding test-user to lorem-ipsum collaborators... done
+`, "output")
+}
+
+func TestPermsCreateAdmin(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/admin/perms/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+	})
+
+	err = cmdr.PermCreate("lorem-ipsum", "test-admin", true)
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Adding test-admin to system administrators... done
+`, "output")
+}
+
+func TestPermsDeleteUser(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+  "uuid": "c4aed81c-d1ca-4ff1-ab89-d2151264e1a3",
+  "id": "lorem-ipsum",
+  "owner": "dolar-sit-amet",
+  "structure": {
+    "cmd": 1
+  },
+  "created": "2016-08-22T17:40:16Z",
+  "updated": "2016-08-22T17:40:16Z"
+}`)
+	})
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/perms", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+	})
+
+	err = cmdr.PermDelete("lorem-ipsum", "test-user", false)
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Removing test-user from lorem-ipsum collaborators... done
+`, "output")
+}
+
+func TestPermsDeleteAdmin(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/admin/perms/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+	})
+
+	err = cmdr.PermDelete("lorem-ipsum", "test-admin", true)
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Removing test-admin from system administrators... done
+`, "output")
+}
